e993b6587459be576ad53893cefdda2fb9b99d43
Isaac <i7chan@uwaterloo.ca>
2022-12-05 00:26:06 -0500

    First commit

---
+++ src/pages/FourOhFour.js
@@ -0,0 +1,13 @@
+import {Route, Switch} from "react-router";
+
+const FourOhFour = function()
+{
+    <Route path="/">
+    <h1 id="sectionHeader">Error 404</h1>
+    <div className="infoContainer">
+        <h2>Looks like that page doesn't exist.</h2>
+    </div>
+  </Route>
+}
+
+export default FourOhFour;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import {Route, Switch} from "react-router";
+
+const Home = function()
+{
+    <Route path="/">
+    <h1 id="sectionHeader">Wow so much Stuff</h1>
+    <div className="infoContainer">
+      <p>
+        A lot of stuff in here.
+      </p>
+    </div>
+  </Route>
+}
+
+export default Home;
